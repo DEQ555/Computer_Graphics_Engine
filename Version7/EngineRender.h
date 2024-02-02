398dbfef800cc28ce933bca1e11c50677f132ce3
DEQ555 <129779702+DEQ555@users.noreply.github.com>
2024-02-02 06:18:44 -0300

    Add files via upload

---
+++ Version7/EngineRender.h
@@ -0,0 +1,41 @@
+#ifndef __TarjetaVideo_H__
+#define __TarjetaVideo_H__
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void CrearBuffer( const int w, const int h );
+void CerrarBuffer( void );
+
+void GuardarBufferV( const char Name[] );
+
+
+// Apartado de ediciones.
+typedef struct{ unsigned char ARGB[4]; }RGBA_;
+
+RGBA_ _RGBA_(unsigned char R, unsigned char G, unsigned char B, unsigned char A );
+
+void Punto( int x, int y, int c );
+
+void Linea( int x0, int y0, int x1, int y1, int c );
+
+void LineaLerp( int x0, int y0, int x1, int y1, RGBA_ c0, RGBA_ c1 );
+
+void Imagen2D( int x, int y, void *data, int w, int h );
+
+typedef struct{
+	int w,h;
+	int* d;
+}ImageRender;
+
+void ImageRenderLoad( ImageRender *X, const char Name[] );
+void ImageRenderFree( ImageRender *X );
+void ImageRenderDraw( ImageRender *X, int x0, int y0 );
+int  ImageRenderGetP( ImageRender *X, int x0, int y0 );
+void ImageRenderDrawRCT( ImageRender *X, int x0, int y0, int x1, int y1 );
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
